6b06d309d483fc229aaf035e4032c84973ec8a6a
lenconda <i@lenconda.top>
2023-01-22 16:47:31 +0800

    fix: 🐛 app.config.ts

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,10 +2,12 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => {
     const {
-        APP_PORT: port,
+        APP_HOST: host = '0.0.0.0',
+        APP_PORT: port = '3000',
     } = process.env ?? {};
 
     return {
+        host,
         port: parseInt(port, 10),
     };
 });
