3e47df7568f77bc548773dd1587eea96b8b04ea2
Peng Hanlin <i@lenconda.top>
2023-03-14 17:10:52 +0800

    fix: array transformer pipe

--- src/common/transform-array-dto.pipe.ts
+++ src/common/transform-array-dto.pipe.ts
@@ -21,7 +21,7 @@ function createTransformArrayDTOPipe<T>(itemType: Type<T>): Type<PipeTransform> 
 
         public transform(values: T[], metadata: ArgumentMetadata): Promise<any[]> {
             if (!Array.isArray(values)) {
-                return [];
+                return Promise.resolve([] as T[]);
             }
             return Promise.all(values.map((value) => super.transform(
                 value,
